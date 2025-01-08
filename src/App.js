272b5f014b5225ee4cd2268021f04e312ab596ac
shreyasraj-001 <shreyasraj735@gmail.com>
2025-01-09 00:36:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+// import Navba from './components/Navbar';
 
 function App() {
+  const [color, setColor] = useState("#4B4BC3");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="w-full h-screen duration-200 style"
+      style={{ backgroundColor: color }}
+      
+    >
+     <div className="text-center text-white font-bold text-2xl px-40 py-50">
+       Background is  {color} 
+      </div>
+
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-full px-3 py-2">
+          <button onClick={()=>{setColor("Red")}} className="outline-none px-4 py-1 rounded-full text-white font-bold shadow-lg " style={{backgroundColor:"Red"}}>Red</button>
+          <button onClick={()=>{setColor("rgb(55 100 214)")}} className="outline-none px-4 py-1 rounded-full text-white font-bold shadow-lg " style={{backgroundColor:"rgb(55 100 214)"}}>Blue</button>
+          <button onClick={()=>{setColor("#56a8ff")}} className="outline-none px-4 py-1 rounded-full text-white font-bold shadow-lg " style={{backgroundColor:"#56a8ff"}}>Sky </button>
+          <button onClick={()=>{setColor("#ff1ad3")}} className="outline-none px-4 py-1 rounded-full text-white font-bold shadow-lg " style={{backgroundColor:"#ff1ad3"}}>Pink</button>
+          <button onClick={()=>{setColor("#2aa63f")}} className="outline-none px-4 py-1 rounded-full text-white font-bold shadow-lg " style={{backgroundColor:"#2aa63f"}}>Green</button>
+          
+
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+// import React, { useState } from 'react'
+
+// const Navbar = () => {
+
+//   let [counter, setCounter]= useState(10);
+
+//   const addValue = ()=> {
+//     setCounter(counter+1);
+//   }
+//   const remValue =()=> {
+//     setCounter(counter-1);
+//   }
+
+//   return (
+//     <div>
+//       <p>Conter {counter}</p>
+//       <button
+//       onClick={addValue}>Add Counter </button>
+//       <br />
+//       <br />
+//       <button
+//       onClick={remValue}>remove Counter </button>
+
+//     </div>
+//   )
+// }
+
+// export default Navbar
